55b614231e1cf861c52a44029df298a2fb3f99c2
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-01-24 21:47:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let result=[];
+	let subarray=[];
+	let pre=0;
+	for(let i=0;i<arr.length;i++){
+		if(pre+arr[i]>n){
+			result.push(subarray);
+			subarray=[];
+			pre=0;
+		}
+		subarray.push(arr[i]);
+		pre+=arr[i];
+		
+	}
+	if(subarray>0){
+		result.push(subarray);
+	}
+	return result;
 };
 
 const n = prompt("Enter n: ");
